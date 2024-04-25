633f230839cf355242eaa09cb373499633d61d56
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-25 11:18:14 -0400

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "azul-claro": "#285b85",
+      },
+    },
   },
   plugins: [],
 };
